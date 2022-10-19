364caf67966c716b9c7c0797c95288997d4fd7c9
Rodevp <rodriguezrodrigor12@gmail.com>
2022-10-19 16:14:05 -0500

    layout inicial y estilos basicos :white_check_mark:

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.taskContainer}>
+        
+        <Text style={styles.title}> Lista de tareas </Text>
+        
+        <View style={styles.items}>
+
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  taskContainer: {
+    paddingTop: 75,
+    paddingHorizontal: 20
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+
+  }
 });
